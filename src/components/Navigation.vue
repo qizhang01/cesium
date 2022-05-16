9ad30e93246781eec2e62c4b519a07a50a3fa69b
qizhang01 <zhishui120@sina.com>
2022-05-16 16:22:21 +0800

    删除不必要的菜单

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -9,33 +9,11 @@
                 text-color="#fff"
                 active-text-color=rgba(47,79,79,0.5) style="border-bottom: solid 0px #e6e6e6;">
             <el-menu-item index="1">菜  单</el-menu-item>
-            <el-submenu index="2">
-                <template slot="title">测  量</template>
-                <el-menu-item index="2-1" @click="drawLineMeasure">长度测量</el-menu-item>
-                <el-menu-item index="2-2" @click="drawAreaMeasure">面积测量</el-menu-item>
-                <el-menu-item index="2-3" @click="drawTrianglesMeasure">三角测量</el-menu-item>
-                <el-menu-item index="2-4" @click.stop="">高度测量</el-menu-item>
-                <el-menu-item index="2-5" @click="CleardrawMeasure">清除测量</el-menu-item>
-            </el-submenu>
-            <el-submenu index="3">
-                <template slot="title">分  析</template>
-                <el-menu-item index="3-1" @click="Loadmodel">模型加载</el-menu-item>
-                <el-menu-item index="3-2">可视域分析</el-menu-item>
-                <el-menu-item index="3-3">三角测量</el-menu-item>
-                <el-menu-item index="3-4">几何分析</el-menu-item>
-                <el-menu-item index="3-5">淹没分析</el-menu-item>
-            </el-submenu>
-            <el-submenu index="4">
-                <template slot="title">动态分析</template>
-                <el-menu-item index="4-1" @click="PorfileAnaly">剖面分析</el-menu-item>
-            </el-submenu>
-            <el-menu-item index="5" @click="restorationViewer">复  位</el-menu-item>
             <el-menu-item index="6" @click="OpenMapbox">MapboxGL</el-menu-item>
         </el-menu>
     </div>
 </template>
 <script>
-    import Measure from  "../js/Measure/cesium-measure"
     import TilesLayerManager from "../js/Entity/LoadTilesLayer"
     import LoadTilesLayer from "../js/Entity/LoadTilesLayer";
     import ViewerBase from "../js/Scene/ViewerBase"
@@ -48,7 +26,6 @@
             return {
                 activeIndex: '1',
                 activeIndex2: '1',
-                measure:null,
                 clampToGround:true,
 
             };
@@ -59,35 +36,7 @@
             handleSelect(key, keyPath) {
                 console.log(key, keyPath);
             },
-            initMeasure(){
-                this.measure = new Measure(window.Scene.viewer);
-                this.clampToGround = true;
 
-            },
-            drawLineMeasure(){
-                this.initMeasure();
-                let that = this;
-                that.measure.drawLineMeasureGraphics({
-                    clampToGround: that.clampToGround,
-                    callback: () => { }
-                })
-            },
-            drawAreaMeasure(){
-                let that = this;
-                that.measure.drawAreaMeasureGraphics({
-                    clampToGround: that.clampToGround,
-                    callback: () => { }
-                })
-            },
-            drawTrianglesMeasure(){
-                this.measure.drawTrianglesMeasureGraphics({
-                    callback: () => { }
-                })
-
-            },
-            CleardrawMeasure(){
-                this.measure._drawLayer.entities.removeAll();
-            },
             Loadmodel(){
                 let tilesLaye = new LoadTilesLayer(window.Scene.viewer);
                 //tilesLaye.AddTilesLayer(window.EarthBaseConfig.TilesModel);
